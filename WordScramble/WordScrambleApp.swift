9277556e86a9b7e035dfba2aabb35bb2efc2dd25
Arjun Devnani <arjun.devnani@queensu.ca>
2021-12-30 18:05:37 -0500

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Arjun on 2021-04-27.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
